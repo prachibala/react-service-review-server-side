125f891c7bcc8dbf14c6ddcf0b72c59493ebda09
Prachi Bala <prachibala330@gmail.com>
2022-11-11 21:25:30 +0600

    v-0.5

--- index.js
+++ index.js
@@ -12,70 +12,150 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.36vzmly.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverApi: ServerApiVersion.v1,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
 });
 
 // JWT Token Verify
-const jwtVerify = (req, res, next) => {};
 
-app.post("/jwt", (req, res) => {
-    const user = req.body;
-    const token = jwt.sign(user, process.env.SECRET_ACCESS_TOKEN, {
-        expiresIn: "2h",
-    });
-    res.send({ token });
-});
+const verifyJWT = (req, res, next) => {
+	const authHeader = req.headers.authorization;
+	if (!authHeader) {
+		return res.status(401).send({ message: "Unauthorized Access" });
+	}
+	const token = authHeader.split(" ")[1];
+	jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, function (err, decoded) {
+		if (err) {
+			return res.status(401).send({ message: "Unauthorized Access" });
+		}
+
+		req.decoded = decoded;
+		next();
+	});
+};
 
 async function run() {
-    try {
-        const menuCollection = client.db("snackbox").collection("menus");
+	try {
+		const menuCollection = client.db("snackbox").collection("menus");
+		const reviewCollection = client.db("snackbox").collection("reviews");
 
-        app.get("/top-rated-menus", async (req, res) => {
-            const query = {};
-            const cursor = menuCollection
-                .find(query)
-                .sort({ ratings: -1 })
-                .limit(3);
-            const menus = await cursor.toArray();
-            res.send(menus);
-        });
+		app.post("/jwt", (req, res) => {
+			const user = req.body;
+			const token = jwt.sign(user, process.env.SECRET_ACCESS_TOKEN, {
+				expiresIn: "2h",
+			});
+			res.send({ token });
+		});
 
-        app.get("/menus", async (req, res) => {
-            const query = {};
-            const cursor = menuCollection.find(query);
-            const menus = await cursor.toArray();
-            res.send(menus);
-        });
-        app.get("/menus/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const menu = await menuCollection.findOne(query);
-            res.send(menu);
-        });
-        // post api
-        app.post("/add-menu", async (req, res) => {
-            const menu = req.body;
-            const ratings = 0;
-            const result = await menuCollection.insertOne({
-                ...menu,
-                ratings: ratings,
-            });
+		app.get("/top-rated-menus", async (req, res) => {
+			const query = {};
+			const cursor = menuCollection
+				.find(query)
+				.sort({ ratings: -1 })
+				.limit(3);
+			const menus = await cursor.toArray();
+			res.send(menus);
+		});
 
-            res.send(result);
-        });
-    } finally {
-    }
+		app.get("/menus", async (req, res) => {
+			const query = {};
+			const cursor = menuCollection.find(query);
+			const menus = await cursor.toArray();
+			res.send(menus);
+		});
+		app.get("/menus/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: ObjectId(id) };
+			const menu = await menuCollection.findOne(query);
+			res.send(menu);
+		});
+		// post api
+		app.post("/add-menu", verifyJWT, async (req, res) => {
+			const menu = req.body;
+			const ratings = 0;
+			const result = await menuCollection.insertOne({
+				...menu,
+				ratings: ratings,
+				ratingsCount: [0, 0, 0, 0, 0],
+				totalRatingsCount: 0,
+			});
+
+			res.send(result);
+		});
+
+		app.post("/add-review", verifyJWT, async (req, res) => {
+			const review = req.body;
+			const result = await reviewCollection.insertOne(review);
+			const addedReview = await reviewCollection.findOne({
+				_id: result.insertedId,
+			});
+			const menu = await menuCollection.findOne({
+				_id: ObjectId(addedReview.menu.menuId),
+			});
+
+			menu.ratingsCount[addedReview.rating - 1] =
+				menu.ratingsCount[addedReview.rating - 1] + 1;
+
+			const newRatings =
+				(menu.ratingsCount[0] * 1 +
+					menu.ratingsCount[1] * 2 +
+					menu.ratingsCount[2] * 3 +
+					menu.ratingsCount[3] * 4 +
+					menu.ratingsCount[4] * 5) /
+				(menu.totalRatingsCount + 1);
+
+			const updateMenu = await menuCollection.updateOne(
+				{
+					_id: ObjectId(addedReview.menu.menuId),
+				},
+				{
+					$inc: {
+						totalRatingsCount: 1,
+						[`ratingsCount.${addedReview.rating - 1}`]: 1,
+					},
+					$set: {
+						ratings: newRatings,
+					},
+				}
+			);
+			// console.log(updateMenu);
+			res.send(result);
+		});
+
+		app.delete("/delete-review/:id", async (req, res) => {
+			const id = req.params.id;
+			const deleteReview = await reviewCollection.deleteOne({
+				_id: ObjectId(id),
+			});
+			res.send(deleteReview);
+		});
+
+		app.get("/reviews-by-menu/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { "menu.menuId": id };
+			const reviews = await reviewCollection.find(query).toArray();
+			res.send(reviews);
+		});
+
+		app.get("/reviews-by-user/:id", verifyJWT, async (req, res) => {
+			const id = req.params.id;
+			const reviews = await reviewCollection
+				.find({ "reviewBy.userId": id })
+				.toArray();
+			res.send(reviews);
+		});
+	} finally {
+	}
 }
 run().catch((error) => {
-    console.error(error);
+	console.error(error);
 });
 
 app.get("/", (req, res) => {
-    res.send("Hello World! is running");
+	res.send("Hello World! is running");
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+	console.log(`Example app listening on port ${port}`);
 });
